72e399266390180c8ebb10ca305330d0b5527453
user <user@example.com>
2025-09-06 08:54:34 +0900

    [xyplorer] 1st commit

--- dotfiles/_scripts/w2a_copy_dotfiles.bat
+++ dotfiles/_scripts/w2a_copy_dotfiles.bat
@@ -36,13 +36,8 @@ set "FILES[16]=Microsoft.PowerShell_profile.ps1|%USERPROFILE%\Documents\PowerShe
 set "FILES[17]=profile.ps1|%USERPROFILE%\Documents\PowerShell\profile.ps1
 set "FILES[18]=config\nushell\config.nu|%USERPROFILE%\.config\nushell\config.nu
 set "FILES[19]=config\nushell\config.nu|%USERPROFILE%\.config\nushell\env.nu
-set "FILES[20]=xyplorer\XYplorer.ini|%APPDATA%\XYplorer\XYplorer.ini
-set "FILES[21]=xyplorer\tag.dat|%APPDATA%\XYplorer\tag.dat
-set "FILES[22]=xyplorer\ks.dat|%APPDATA%\XYplorer\ks.dat
-set "FILES[23]=xyplorer\catalog.dat|%APPDATA%\XYplorer\Catalogs\catalog.dat
-set "FILES[24]=xyplorer\pane1.ini|%APPDATA%\XYplorer\Panes\1\t\pane.ini
-set "FILES[25]=xyplorer\pane2.ini|%APPDATA%\XYplorer\Panes\2\t\pane.ini
-set "MAX_IDX=25
+set "FILES[20]=xyplorer|%VAULT_HOME%\tool\xyplorer_full_noinstall\Data
+set "MAX_IDX=20
 
 rem set "FILES[10]=config\nvim-shared|%USERPROFILE%\.config\nvim-win\lua\shared"
 REM            @("config",		               	       "$Home\.config\bat\config"),
