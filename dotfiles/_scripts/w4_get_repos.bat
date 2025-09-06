49015a81ec4d58d5eae51147e57dfea82c819300
user <user@example.com>
2025-09-06 14:55:41 +0900

    [script] recommended github repos

--- dotfiles/_scripts/w4_get_repos.bat
+++ dotfiles/_scripts/w4_get_repos.bat
@@ -3,9 +3,21 @@ setlocal EnableExtensions EnableDelayedExpansion
 
 ghq get yuzucha16/env
 
+rem c++ samples
+ghq get bareflank/static_interface_pattern
+ghq get skypjack/entt
+ghq get microsoft/proxy
+ghq get foonathan/type_safe
+ghq get mpusz/mp-units
+ghq get cpp-best-practices/gui_starter_template
+
+rem dev settings
 rem ghq get yuzuch16/env
 rem ghq get yuzuch16/adv360-pro-zmk
-rem ghq get yuzuch16/env
+rem ghq get yuzuch16/dotfiles
+
+rem Autosar samples
+ghq get inniyah/arccore
 
 :END
 pause
