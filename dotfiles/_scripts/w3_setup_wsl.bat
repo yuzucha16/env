a941551659297f038ec588b7d672ca3335bc6a8e
user <user@example.com>
2025-09-06 19:33:40 +0900

    [script] add ctrl-capslock & notepadpp

--- dotfiles/_scripts/w3_setup_wsl.bat
+++ dotfiles/_scripts/w3_setup_wsl.bat
@@ -1,8 +1,18 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-wsl --list --online
-wsl --install -d Ubuntu-24.04
+rem --- 必須コンポーネント（WSL2に必要） ---
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:HypervisorPlatform /all /norestart
+
+rem --- ハイパーバイザ起動を有効化 ---
+bcdedit /set hypervisorlaunchtype auto
+
+echo Please reboot now. After reboot, run the following manually:
+echo   wsl --update
+echo   wsl --list --online
+echo   wsl --install -d Ubuntu-24.04
 
 :END
 pause
